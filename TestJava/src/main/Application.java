b5ee5b86a2e5223f77410957270eb0e459981535
atanaltay <atanaltay@gmail.com>
2022-10-03 14:38:21 +0300

    test commit

---
+++ TestJava/src/main/Application.java
@@ -0,0 +1,26 @@
+package main;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Person p1 = new Person(0,"altug", "tanaltay");
+		Person p2 = new Person(1,"ali","korkmaz");
+		
+		
+		
+		System.out.println(p1.getName());
+		System.out.println(p2.getName());
+
+		
+		
+		System.out.println(p1);
+		
+		
+		
+	}
+	
+	
+	
+	
+}
