94fb01823b564df452827cf4464447963d5f0e46
atanaltay <atanaltay@gmail.com>
2023-10-03 13:51:12 +0300

    cmt1

---
+++ TestJava1/src/main/MainClass.java
@@ -0,0 +1,42 @@
+package main;
+
+public class MainClass {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java....");
+		
+		int x = 20;
+		int y = 300;
+		
+		
+		int result = x+y;
+		
+		byte b1 = 10;
+		byte b2 = 11;
+		
+		byte bSum = (byte)(b1+b2);
+		
+		
+		double d1 = 10.23;
+		
+		
+		int x22 = (int)d1;
+		
+		System.out.printf("Number is %d\n",x22);
+	
+		String name ="Jackson";
+		System.out.printf("Her name is %s\n",name);
+		
+		
+		int someNum = (int)(23.3 + 23.4);
+		
+		System.out.println("Some num + " + someNum);
+		
+		
+		
+		
+	}
+	
+	
+}
